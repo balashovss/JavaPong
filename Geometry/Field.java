ee7f473315ace49e9d896e1d20f9c6132410e6c2
Semakiller <semen.balashov.02@gmail.com>
2024-10-24 18:54:28 +0300

    Initial commit

---
+++ Geometry/Field.java
@@ -0,0 +1,11 @@
+package Geometry;
+public class Field {
+    private int width = 80;
+    private int height = 25;
+    private char [][] matrix = new char[this.height][this.width];
+
+}
+class Point {
+    private int x = 0;
+    private int y = 0;
+}
---
+++ ballstuff/Ball.java
@@ -0,0 +1,8 @@
+package ballstuff;
+import java.util.Scanner;
+public class Ball {
+    private int x = 0;
+    private int y = 0;
+    private int speedX = 0;
+    private int speedY = 0;
+}
